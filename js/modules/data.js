72a049ca182174f43eb8528a0ff473ba5dbf2828
Tuturo <olenberg.vadim@gmail.com>
2023-01-27 12:31:02 +0300

    Правит функцию поиска уникальных элементов в массиве

---
+++ js/modules/data.js
@@ -0,0 +1,62 @@
+const AUTHORS = [
+    'img/avatars/user01.png',
+    'img/avatars/user02.png',
+    'img/avatars/user03.png',
+    'img/avatars/user04.png',
+    'img/avatars/user05.png',
+    'img/avatars/user06.png',
+    'img/avatars/user07.png',
+    'img/avatars/user08.png',
+    'img/avatars/user09.png',
+    'img/avatars/user10.png',
+];
+
+const TITLES = [
+    'Вариант вашей мечты!',
+    'От сердца отрываю!',
+    'Звоните, а то арендуют!',
+    'Только ирландцам!',
+    'Можно с животными!',
+    'Сдаю комнату',
+];
+
+const TYPES = [
+    'palace',
+    'flat',
+    'house',
+    'bungalow',
+];
+
+const TIMES = [
+    '12:00',
+    '13:00',
+    '14:00',
+];
+
+const FEATURE_LIST = [
+    'wifi',
+    'dishwasher',
+    'parking',
+    'washer',
+    'elevator',
+    'conditioner',
+];
+
+const DESCRIPTIONS = [
+    'Вариант мечты!',
+    'Сам жил 10 лет, идеально.',
+    'Отрываю от сердца!',
+    'Из окна открывается потрясающий вид на другое здание.',
+    'Консьерж в доме, который не пустит вас после девяти вечера.',
+    'Есть куда поставить машину!',
+    'Большая кладовая в подарок.',
+    'Бесплатные завтраки всего за 150 рублей.'
+];
+
+const PHOTOS = [
+    'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+    'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+    'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
+];
+
+export {AUTHORS, TITLES, TYPES, TIMES, FEATURE_LIST, DESCRIPTIONS, PHOTOS};
---
+++ js/modules/getDataArray.js
@@ -0,0 +1,59 @@
+import {getRandomInt, getRandomIntFloat} from './util.js';
+import {AUTHORS, TITLES, TYPES, TIMES, FEATURE_LIST, DESCRIPTIONS, PHOTOS} from './data.js';
+
+
+const getRandomArrayElement = (elements) => {
+    return elements[getRandomInt(0, elements.length - 1)];
+};
+
+const getUniqArrayElements = (elements) => {
+    let result = [];
+    let randomValue = getRandomInt(0, elements.length - 1);
+
+    for (let i = 0; i <= randomValue; i++) {
+        let k = getRandomInt(i, elements.length - 1);
+        result.push(elements[k]);
+        [elements[i], elements[k]] = [elements[k], elements[i]];
+    };
+
+    return result;
+};
+
+const getAuthor = () => {
+    return {
+        author: {
+            avatar: getRandomArrayElement(AUTHORS),
+        }
+    };
+};
+
+const getOffer = () => {
+    return {
+        offer: {
+            title: getRandomArrayElement(TITLES),
+            address: '{{location.x}}, {{location.y}}',
+            price: `${getRandomInt(10000, 50000)} руб./месяц`,
+            type: getRandomArrayElement(TYPES),
+            rooms: getRandomInt(1, 7),
+            guests: getRandomInt(2, 14),
+            checkin: getRandomArrayElement(TIMES),
+            checkout: getRandomArrayElement(TIMES),
+            features: getUniqArrayElements(FEATURE_LIST),
+            description: getUniqArrayElements(DESCRIPTIONS).join(' '),
+            photos: getUniqArrayElements(PHOTOS),
+        },
+    };
+};
+
+const getLocation = () => {
+    return {
+        location: {
+            x: getRandomIntFloat(35.65000, 35.70000, 5),
+            y: getRandomIntFloat(139.70000, 139.80000, 5),
+        },
+    }
+};
+
+const NEW_ARRAY = new Array(10).fill(null).map(() => Object.assign({}, getAuthor(), getOffer(), getLocation()));
+
+export {NEW_ARRAY};
