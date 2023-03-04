c9e82d46f053d9659ffee4ce6bcb0366661ddee3
Tuturo <olenberg.vadim@gmail.com>
2023-03-04 20:23:11 +0300

    Добавляет валидацию полю #price

--- js/modules/validation.js
+++ js/modules/validation.js
@@ -1,9 +1,10 @@
-import { MIN_TITLE_LENGTH, MAX_TITLE_LENGTH } from './data.js';
+import { MIN_TITLE_LENGTH, MAX_TITLE_LENGTH, MAX_PRICE_VALUE } from './data.js';
 
 const adForm = document.querySelector('.ad-form');
 const adTitle = adForm.querySelector('#title');
+const adPrice = adForm.querySelector('#price');
 
-const checkTitleValidation = () => {
+const titleValidation = () => {
     const valueLength = adTitle.value.length;
 
     if (valueLength < MIN_TITLE_LENGTH) {
@@ -17,4 +18,22 @@ const checkTitleValidation = () => {
     adTitle.reportValidity();
 };
 
-adTitle.addEventListener('input', checkTitleValidation);
+adTitle.addEventListener('input', titleValidation);
+
+const priceValidation = () => {
+    const priceValue = Number(adPrice.value);
+
+    if (adPrice.value.length === 0) {
+        adPrice.setCustomValidity('Обязательное поле');
+    } else if (!priceValue) {
+        adPrice.setCustomValidity('Это числовое поле');
+    } else if (priceValue > MAX_PRICE_VALUE) {
+        adPrice.setCustomValidity('Один миллион — максимальное значение');
+    } else {
+        adPrice.setCustomValidity('');
+    };
+
+    adPrice.reportValidity();
+};
+
+adPrice.addEventListener('input', priceValidation);
